6617af548a31885f9832162281aaf4f9aa2b4fdf
edger-asiimwe <edgerasiimwe@gmail.com>
2024-04-12 22:53:50 +0300

    Updated User Model

--- ofoads/models.py
+++ ofoads/models.py
@@ -1,59 +1,27 @@
 from ofoads import db
-
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):  
+
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    role = db.Column(db.String(50), nullable=False)  # Role can be 'admin', 'restaurant', or 'client'
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String, nullable=False)
+    role = db.Column(db.String(50), nullable=False)  # ['admin', 'restaurant', client]
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    @property
+    def password(self):
+        raise AttributeError('Password cannot be accessed') 
     
-    def __repr__(self) -> str:
-        return 'User {}'.format(self.usernames)
-    
-class Admin(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-class Restaurant(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    location = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-class Food(db.Model):
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    preparation_time = db.Column(db.Integer, nullable=False)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), primary_key=True)
-    id = db.Column(db.Integer, primary_key=True)
-    restaurant = db.relationship('Restaurant', backref=db.backref('foods', lazy=True))
-
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    food_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
-    restaurant_id = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.String(100), nullable=False)  # Status can be 'Pending', 'Completed', etc.
-
-    @validates('restaurant_id')
-    def validate_restaurant_id(self, key, restaurant_id):
-        # Perform validation to ensure that all food items in the order share the same restaurant ID
-        if self.food_id:  # Check if food_id is present
-            food = Food.query.get(self.food_id)
-            if food and food.restaurant_id != restaurant_id:
-                raise ValueError("All food items in the order must belong to the same restaurant")
-        return restaurant_id
-
-class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-
-
-
-
+    def __repr__(self):
+        return 'User: {} - {}'.format(self.email, self.role)
