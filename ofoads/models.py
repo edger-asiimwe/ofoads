0b1b6137fe339f120392b3a2c3d5c20a34d35f73
jonathan-ndayisenga <brucendayisenga22@gmail.com>
2024-04-12 20:31:23 +0300

    models.py

--- ofoads/models.py
+++ ofoads/models.py
@@ -8,6 +8,42 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-
+    role = db.Column(db.String(50), nullable=False)  # Role can be 'admin', 'restaurant', or 'client'
     def __repr__(self) -> str:
         return 'User {}'.format(self.usernames)
+    
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    preparation_time = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    restaurant = db.relationship('Restaurant', backref=db.backref('foods', lazy=True))
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    food_id = db.Column(db.Integer, db.ForeignKey('food.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(100), nullable=False)  # Status can be 'Pending', 'Completed', etc.
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+
+
+
