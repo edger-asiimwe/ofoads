18db0d32554e6756dfe212bfe849b872c8bc1a57
edger-asiimwe <edgerasiimwe@gmail.com>
2024-04-15 17:20:55 +0300

    Added Client Model

--- ofoads_app/client/routes.py
+++ ofoads_app/client/routes.py
@@ -1,7 +1,14 @@
 from flask import render_template
 
 from . import client
+from ..models import Client
+from ..forms import ClientRegistrationForm
 
 @client.route('/dashboard')
 def dashboard():
     return render_template('client/dashboard.html')
+
+
+@client.route('/register')
+def dashboard():        
+    return "Success"
